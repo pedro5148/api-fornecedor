201e9a17f553422c87e8330addb234f20cf971c9
Pedro Eugenio <enviaparaopedro@gmail.com>
2025-01-20 22:37:48 -0300

    Tratamento de erro para ID

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -1,9 +1,60 @@
-export default (err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
+import AppErro from './../utils/appError.js'
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    })
+export const handleCastErrorDB = (err) => {
+    const message = `Informado ID invalido ${err.path}: ${err.value}`
+    return new AppErro(message, 400)
+}
+
+export const sendErrorDev = (error, res) => {
+    res.status(error.statusCode).json({
+        status: error.status,
+        error: error,
+        message: error.message,
+        stack: error.stack
+    });
+};
+
+export const sendErrorProd = (error, res) => {
+    // Erros operacionais, sao erros conhecidos e podem ser retornados ao cliente.
+    if (error.isOperational) {
+        res.status(error.statusCode).json({
+            status: error.status,
+            message: error.message
+        })
+
+        //Erros de programacao, retorna mensagem de erro generico
+    } else { 
+        console.log('Error: ', error)
+        res.status(500).json({
+            status: 'error',
+            message: 'Alguma coisa deu errado...'
+        })
+    }
+}
+
+export default (error, req, res, next) => {
+
+    error.statusCode = error.statusCode || 500;
+    error.status = error.status || 'error';
+
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(error, res);
+    } else if (process.env.NODE_ENV === 'production') {
+
+        let err = { ...error, 
+            name: error.name,
+            message: error.message,
+            stack: error.stack,
+            path: error.path, // Inclui `path` para CastError
+            value: error.value, 
+        }
+        if (err.name === 'CastError') err = handleCastErrorDB(err)
+
+        sendErrorProd(err, res);
+    }
+
+    // res.status(err.statusCode).json({
+    //     status: err.status,
+    //     message: err.message
+    // })
 }
--- src/controllers/fornecedorController.js
+++ src/controllers/fornecedorController.js
@@ -1,28 +1,31 @@
 import { FornecedorService } from '../service/fornecedorService.js';
 const fornecedorService = new FornecedorService();
 import catchAsync from './../utils/catchAsync.js'
+import appErro from './../utils/appError.js'
 
 //Route Handlers
 //exports.getAllFornec = async (req, res) => {
 export const getAllFornec = catchAsync(async (req, res) => {
 
-        console.log('Filtros da requisicao:', req.query) //testando filtros
-        const fornec = await fornecedorService.findAll(req.query);
-        res.status(200).json({ 
-            status: 'sucess',
-            requestdAt: req.requestTime,
-            results: fornec.length, 
-            data: {
-                fornec
-            } 
-        })
-  });
+    console.log('Filtros da requisicao:', req.query) //testando filtros
+    const fornec = await fornecedorService.findAll(req.query);
+    res.status(200).json({ 
+        status: 'sucess',
+        requestdAt: req.requestTime,
+        results: fornec.length, 
+        data: {
+            fornec
+        } 
+    })
+});
   
 //exports.getFornecbyId = async (req, res) => {
-export const getFornecbyId = catchAsync(async (req, res) => {
+export const getFornecbyId = catchAsync(async (req, res, next) => {
 
         const fornecedor = await fornecedorService.findById(req.params.id);
         
+        if (!fornecedor) { return next(new appErro('Nenhum fornecedor encontrado com esse ID', 404)) }
+
         res.status(200).json({ 
             status: 'sucess', 
             requestdAt: req.requestTime,
@@ -33,7 +36,7 @@ export const getFornecbyId = catchAsync(async (req, res) => {
 });
 
 //exports.createFornec = async (req, res) => {
-export const createFornec = catchAsync(async (req, res) => {
+export const createFornec = catchAsync(async (req, res, next) => {
 
         const newFornec = await fornecedorService.create(req.body);
 
@@ -47,8 +50,11 @@ export const createFornec = catchAsync(async (req, res) => {
 });
 
 //exports.updateFornec = (req, res) => {
-export const updateFornec = catchAsync(async (req, res) => { 
+export const updateFornec = catchAsync(async (req, res, next) => { 
         const updateFornec = await fornecedorService.update(req.params.id, req.body)
+
+        if (!updateFornec) { return next(new appErro('Nenhum fornecedor encontrado com esse ID', 404)) }
+
         res.status(204).json({ 
             status: 'sucess',
             requestdAt: req.requestTime, 
@@ -59,8 +65,11 @@ export const updateFornec = catchAsync(async (req, res) => {
 });
 
 //exports.deleteFornec = (req, res) => {
-export const deleteFornec = catchAsync(async (req, res) => {
-        await fornecedorService.delete(req.params.id); // nao devolver nada para o cliente (RESTFUL)
+export const deleteFornec = catchAsync(async (req, res, next) => {
+        const deleteFornec = await fornecedorService.delete(req.params.id); // nao devolver nada para o cliente (RESTFUL)
+
+        if (!deleteFornec) { return next(new appErro('Nenhum fornecedor encontrado com esse ID', 404)) }
+
         res.status(204).json({ 
             status: 'sucess',
             data: null
